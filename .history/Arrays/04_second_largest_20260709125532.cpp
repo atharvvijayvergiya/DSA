288b5aaaf30c222cc823ebc496b95f78ed1d2142
Atharv VIjayvergiya <vijayvergiyaatharv@gmail.com>
2026-07-09 13:58:45 +0530

    sorted array added

---
+++ .history/Arrays/04_second_largest_20260709125532.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>> n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    int smallest = arr[0];
+    int second_smallest = INT_MAX;
+
+    for(int i=0;i<n;i++){
+        if(arr[i]<smallest){
+            second_smallest=smallest
+            smallest=arr[i];
+        }
+    }
+
+}
---
+++ .history/Arrays/05_check_sorted_array_20260709132339.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int sorted = 1;
+    for(int i=1;i<n;i++){
+        if(arr[i]>=arr[i-1]){
+            sorted =
+        }
+        else{
+            sorted = 0;
+            break;
+        }
+    }
+    if(sorted == 1)
+
+    return 0;
+}
