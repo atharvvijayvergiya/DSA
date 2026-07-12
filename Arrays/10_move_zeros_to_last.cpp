73f283c6363aa057ae86b8aafa0e18b851452eea
Atharv VIjayvergiya <vijayvergiyaatharv@gmail.com>
2026-07-12 11:10:57 +0530

    new problem

--- Arrays/10_move_zeros_to_last.cpp
+++ Arrays/10_move_zeros_to_last.cpp
@@ -1,5 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
+void swap(int &a, int &b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
+void remove_zeros_atend(int arr[], int n){
+    // step 1: finding the first zero element in the array and pointing its index with j;
+    int j=-1;
+    for(int i=0;i<n;i++){
+        if(arr[i]==0){
+            j = i;
+            break;
+        }
+    }
+    // step 1.1: if there are no zero elements in the arr;
+    if(j==-1){
+        return;
+    }
+    // step 2: to iterate the array from the first zero element to the last element of the array
+    for(int i=j+1;i<n;i++){
+        if(arr[i]!=0){
+            swap(arr[i],arr[j]);
+            j++;
+        }
+    }
+}
 int main(){
     int n;
     cin>>n;
@@ -7,6 +33,8 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
+    remove_zeros_atend(arr,n);
+
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
