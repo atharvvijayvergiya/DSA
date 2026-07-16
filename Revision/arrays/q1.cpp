d7a35a5430bb8e03430c72956513d55846266317
Atharv VIjayvergiya <vijayvergiyaatharv@gmail.com>
2026-07-16 22:57:21 +0530

    largest element revised

---
+++ Revision/arrays/q1.cpp
@@ -0,0 +1,19 @@
+//largest element
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int max = 0;
+    for(int i=0;i<n;i++){
+        if(a[i]>a[max]){
+            max=i;
+        }
+    }
+    cout<<a[max];
+    return 0;
+}
