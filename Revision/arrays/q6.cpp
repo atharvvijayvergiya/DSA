e9e5827e754179ca443cbcbadff9439ea1c1fdef
Atharv VIjayvergiya <vijayvergiyaatharv@gmail.com>
2026-07-17 14:12:40 +0530

    left rotate by d places optimal solution revised and coded

--- Revision/arrays/q6.cpp
+++ Revision/arrays/q6.cpp
@@ -1,6 +1,11 @@
 // left rotate by d places;
 #include<bits/stdc++.h>
 using namespace std;
+void leftRotate(int a[],int n, int d){
+    reverse(a,a+d);
+    reverse(a+d,a+n);
+    reverse(a,a+n);
+}
 int main(){
     int n;
     cin>>n;
@@ -10,16 +15,26 @@ int main(){
     }
     int d;
     cin>>d;
-    int temp[d];
-    for(int i=0;i<d;i++){
-        temp[i] = a[i];
-    }
-    for(int i=d;i<n;i++){
-        a[i-d]=a[i];
+    d = d%n;
+    leftRotate(a,n,d);
+    for(int i=0;i<n;i++){
         cout<<a[i]<<" ";
-    }
-    for(int i = 0;i<d;i++){
-        cout<<temp[i]<<" ";
     }
     return 0;
 }
+
+
+
+
+//Brute Force coded:
+     //int temp[d];
+    // for(int i=0;i<d;i++){
+    //     temp[i] = a[i];
+    // }
+    // for(int i=d;i<n;i++){
+    //     a[i-d]=a[i];
+    //     cout<<a[i]<<" ";
+    // }
+    // for(int i = 0;i<d;i++){
+    //     cout<<temp[i]<<" ";
+    // }
