be867cf116ed907ff8a675f0076dbba0ed15755a
Atharv VIjayvergiya <vijayvergiyaatharv@gmail.com>
2026-07-17 13:48:41 +0530

    remove duplicates, optimal sol

--- Revision/arrays/q4.cpp
+++ Revision/arrays/q4.cpp
@@ -1,4 +1,4 @@
-//removing duplicates
+//removing duplicates from a sorted array:
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
@@ -8,10 +8,21 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>a[i];
     }
-    //made set and printed the set values, duplicates removed
-    set<int> s(a,a+n);
-    for(auto it:s){
-        cout<<it<<" ";
+    //optimization:
+    int i = 0;
+    for(int j=1;j<n;j++){
+        if(a[j] != a[i]){
+            a[i+1] = a[j];
+            i++;
     }
+}
+    for(int i=0;i<n;i++){
+        cout<<a[i];
+    }
+    // //made set and printed the set values, duplicates removed
+    // set<int> s(a,a+n);
+    // for(auto it:s){
+    //     cout<<it<<" ";
+    // }
     return 0;
 }
