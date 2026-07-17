bddc2b464482e93b6bf2c642dea75a529f194c42
Atharv VIjayvergiya <vijayvergiyaatharv@gmail.com>
2026-07-17 14:00:44 +0530

    left rotate by d places brute forced revised

---
+++ Revision/arrays/q6.cpp
@@ -0,0 +1,25 @@
+// left rotate by d places;
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int d;
+    cin>>d;
+    int temp[d];
+    for(int i=0;i<d;i++){
+        temp[i] = a[i];
+    }
+    for(int i=d;i<n;i++){
+        a[i-d]=a[i];
+        cout<<a[i]<<" ";
+    }
+    for(int i = 0;i<d;i++){
+        cout<<temp[i]<<" ";
+    }
+    return 0;
+}
