b22b4c451cccc3bebdb67e95a075cc8b1aeccb09
Atharv VIjayvergiya <vijayvergiyaatharv@gmail.com>
2026-07-17 10:51:11 +0530

    second largest brute force solved

--- Revision/arrays/q2.cpp
+++ Revision/arrays/q2.cpp
@@ -8,17 +8,27 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>a[i];
     }
-    int largest = a[0];
-    int slargest=-1;
-    for(int i=0;i<n;i++){
-        if(a[i]>largest){
-            slargest=largest;
-            largest=a[i];
+  //brute approach;
+    //converting array into set for sorted as well as unique elements:
+  set <int> s(a,a+n);
+    //again convert the set into an array using vector:
+    vector <int> b(s.begin(),s.end());
+    // we are doing -2 becuase the index starts with zero, so to find the 
+    //largest element we already have it at 4th index so we do -2 to get the second 
+    //largest elment:
+    if(b.size()==1){
+        for(int i=0;i<b.size();i++){
+            cout<<b[i]<<" ";
         }
-        else if(a[i]>slargest && a[i]<largest){
-            slargest=a[i];
-        }
+        cout<<endl;
+        cout<<b[b.size()];
     }
-    cout<<slargest;
-    return 0;
+    else{
+        for(int i=0;i<b.size();i++){
+            cout<<b[i]<<" ";
+        }
+        cout<<endl;
+    cout<<b[b.size()-2];
+    } 
+   return 0;
 }
