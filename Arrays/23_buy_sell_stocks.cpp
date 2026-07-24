8be86f386259c29c48132b0ac68bbe620d2b7181
Atharv VIjayvergiya <vijayvergiyaatharv@gmail.com>
2026-07-24 11:03:02 +0530

    buy and sell stocks optimal solution added

--- Arrays/23_buy_sell_stocks.cpp
+++ Arrays/23_buy_sell_stocks.cpp
@@ -7,13 +7,21 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    int profit =INT_MIN;
-    int diff = 0;
+    // brute force approach:
+    // int profit =INT_MIN;
+    // int diff = 0;
+    // for(int i=0;i<n;i++){
+    //     for(int j = i+1;j<n-1;j++){
+    //          diff = arr[j] - arr[j-1];
+    //          profit = max(profit,diff);
+    //         }
+    // }
+    int mini = arr[0];
+    int profit = 0;
     for(int i=0;i<n;i++){
-        for(int j = i+1;j<n-1;j++){
-             diff = arr[j] - arr[i];
-             profit = max(profit,diff);
-            }
+        int cost = arr[i] - mini;
+        profit = max(profit,cost);
+        mini = min(mini,arr[i]);
     }
     cout<<profit;
     return 0;
