9eeba644e60eac98e367dc7ce033ee0c9fa8138a
Atharv VIjayvergiya <vijayvergiyaatharv@gmail.com>
2026-07-29 10:01:06 +0530

    lower bound index added

---
+++ Binary Search/03_lower_bound.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int lowerBound(int arr[], int n, int target){
+    int low=0;
+    int high=n-1;
+    int ans=n;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(arr[mid]>=target){
+            ans = mid;
+            high = mid -1;
+        }
+        else{
+            low = mid+1;
+        }
+}
+return ans;
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int target;
+    cin>>target;
+int lower_bound_index = lowerBound(arr,n,target);
+cout<<lower_bound_index;
+    return 0;
+}
