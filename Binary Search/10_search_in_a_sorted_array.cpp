0f16e758d202bc72d9f2109ca7c6a046f44cb710
Atharv VIjayvergiya <vijayvergiyaatharv@gmail.com>
2026-08-01 10:30:01 +0530

    search in rotated array

---
+++ Binary Search/10_search_in_a_sorted_array.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+int bsRotatedArray(int arr[], int n, int target){
+    int low=0;
+    int high=n-1;
+    while(low<=high){
+        int mid = (low+high)/2;
+        if(arr[mid]==target){
+            return mid;
+        }
+        //left sorted
+        else if(arr[low]<=arr[mid]){
+            if(arr[low]<=target && target<=arr[mid]){
+                high = mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        else{
+            if(arr[mid]<=target && target<=arr[high]){
+                low = mid +1;
+            }
+            else{
+                high = mid -1;
+            }
+        }
+    }
+    return -1;
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int target;
+    cin>>target;
+    int ans = bsRotatedArray(arr,n,target);
+    cout<<ans;
+    return 0;
+}
