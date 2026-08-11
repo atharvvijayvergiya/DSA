4c76af86ca9d2d5abb4cf2af2cf65abdfe57b4c3
Atharv VIjayvergiya <vijayvergiyaatharv@gmail.com>
2026-08-11 11:14:46 +0530

    optimal solution added for the same problem

---
+++ Binary Search/14_single_element_in_a_sorted_array_optimal_sol.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+int singleSearch(int arr[] ,int n){
+    int low = 1;
+    int high = n-2;
+    if(arr[0]!=arr[low]){
+        return arr[0];
+    }
+    else if(arr[n-1]!=arr[n-2]){
+        return arr[n-1];
+    }
+
+    while(low<=high){
+        int mid = (low+high)/2;
+        if(arr[mid]!=arr[mid-1] && arr[mid]!=arr[mid+1]){
+            return arr[mid];
+        }
+        if(mid%2==1){
+            if(arr[mid-1]==arr[mid]){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+    }
+    return -1;
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    //fetch;
+    int ans = singleSearch(arr,n);
+    cout<<ans;
+    return 0;
+}
